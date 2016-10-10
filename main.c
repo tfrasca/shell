3a82095e092050c4f70bde99ffea0179a4552eeb
Tyler Frasca <tyfrasc15@gmail.com>
2016-10-10 15:25:49 -0400

    defining methods parseCmd & mystrncpy, however mystrncpy seg faults, due to memory access restrictions

--- main.c
+++ main.c
@@ -12,32 +12,50 @@ main (int argc, char *argv[]) {
 int interactiveMode() { 
   struct Cmd inputCmd;
   int quitFlag=0;
-  char *quit="quit";
-  int i=0;
-  int index=0;
-  char nextWrd[MAXLINESIZE];
+  char quit[4]="quit";
   while(!quitFlag) {
     printf("8[ ");
-    i=0;
-    index=0;
-    while((nextWrd[i]=getchar()) !='\n') {
-      if(nextWrd[i]==' ') {
-        inputCmd.argv[index]=nextWrd;
-        printf("%s",inputCmd.argv[index]); 
-        index++;
-      }
-        printf("%s",nextWrd); 
-      //if(strcmp(inputCmd.argv[index],quit)==0) {
-      //  printf("quit!!!\n");
-      //  quitFlag=1;
-      //}
-      i++;
-    }
-    printf("\n");
+    quitFlag=parseCmd(&inputCmd);
+    break;
   }
   return 1;
 }
 
 int batchMode(int argc, char *argv[0]) {
   return 1;
+}
+
+int readCmd(struct Cmd *inputCmd) {
+  //need to read line -> parse 
+  //while((
+  
+ // while((nextWrd=getchar()) !='\n') {
+ //   if(nextWrd[i]==' ') {
+ //     strcpy(inputCmd.argv[index],nextWrd);
+ //     printf("%s",inputCmd.argv[index]); 
+ //     index++;
+ //   }
+ // }
+  return 1;
+}
+int parseCmd(struct Cmd *inputCmd) {
+  int index=0;
+  char nextWrd[MAXLINESIZE];
+  int i=0;
+  while((nextWrd[i]=getchar()) !='\n') {
+    if(nextWrd[i]==' ') {
+      //inputCmd->argv[index]="doh";
+      mystrncpy(inputCmd->argv[index],nextWrd,i);
+      printf("%s\n",inputCmd->argv[index]);
+    }
+    i++;
+  }
+  return 1;
+}
+
+int mystrncpy(char *dest,char *src,size_t n) {
+  int i=0;
+  for(;i<n;i++) {
+    *(dest+i)=src[i];
+  }
 }
