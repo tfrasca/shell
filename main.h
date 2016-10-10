0d75550bfefb85cda8b22e4b9597840b4367a257
Tyler Frasca <tyfrasc15@gmail.com>
2016-10-10 15:24:39 -0400

    added methods parseCmd, readCmd(prob not use), & mystrncpy

--- main.h
+++ main.h
@@ -1,10 +1,13 @@
 #define MAXLINESIZE 1000
 #define MAXWORDS 300
 
-int interactiveMode();
-int batchMode(int argc, char *argv[0]);
-
 struct Cmd {
   char *argv[MAXWORDS];
   int argc;
 };
+
+int interactiveMode();
+int batchMode(int argc, char *argv[0]);
+int readCmd(struct Cmd *inputCmd);
+int parseCmd(struct Cmd *inputCmd);
+int mystrncpy(char *dest,char *src,size_t n);
