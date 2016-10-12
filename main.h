897d61a3188d4a7f5d0318747f8650cbc298be9c
Tyler Frasca <tyfrasc15@gmail.com>
2016-10-11 20:58:35 -0400

    changed struct, and changed return val of parseCmd

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #define WORDSIZE 100
 
 struct Cmd {
+  char executable[WORDSIZE];
   char argv[MAXWORDS][WORDSIZE];
   int argc;
 };
@@ -10,5 +11,5 @@ struct Cmd {
 int interactiveMode();
 int batchMode(char *batchfile);
 int readCmd(struct Cmd *inputCmd);
-void parseCmd(struct Cmd *inputCmd);
+int parseCmd(struct Cmd *inputCmd);
 int mystrncpy(char *dest,char *src,size_t n);
