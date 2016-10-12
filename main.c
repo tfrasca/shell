50d6992013192d1e28db604789c113d37816caee
Tyler Frasca <tyfrasc15@gmail.com>
2016-10-11 20:57:50 -0400

    trying to improve the parsing

--- main.c
+++ main.c
@@ -20,28 +20,37 @@ int main (int argc, char *argv[]) {
 int interactiveMode() {
   struct Cmd cmd;
   char quit[] = "quit";
+  int processPid;
+  int *status;
+  int quitFlag=0;
 
   do {
+  //promt
     printf("8[ ");
     //parse command
-    parseCmd(&cmd);
-    printf("%s", cmd.argv[0]);
+    quitFlag = parseCmd(&cmd);
+    printf("%s\n",cmd.executable);
+    if(strncmp(cmd.executable,quit,4)==0 || quitFlag) {
+      exit(0);
+    }
 
     //fork child
+    processPid = fork();
+    if ( processPid ==0) {
+      //not taking argv becuase supposed to be char *argv[] we have char argv[][]??
+      execve(cmd.executable,cmd.argv[0],NULL);
+      printf("child\n");
+    }
+    else {
+      printf("adult\n");
+    }
+    waitpid(processPid,status,0);
 
 
     //execute command
     //wait for child to finish
-  } while (!feof(stdin) && strcmp(cmd.argv[0], quit));
+  } while (!feof(stdin) && strncmp(cmd.executable, quit,4)!=0);
 
-  // struct Cmd inputCmd;
-  // int quitFlag=0;
-  // char quit[4]="quit";
-  // while(!quitFlag) {
-  //   printf("8[ ");
-  //   quitFlag=parseCmd(&inputCmd);
-  //   break;
-  // }
   return 1;
 }
 
@@ -50,37 +59,35 @@ int batchMode(char *batchfile) {
 }
 
 int readCmd(struct Cmd *inputCmd) {
-  //need to read line -> parse 
-  //while((
-  
- // while((nextWrd=getchar()) !='\n') {
- //   if(nextWrd[i]==' ') {
- //     strcpy(inputCmd.argv[index],nextWrd);
- //     printf("%s",inputCmd.argv[index]); 
- //     index++;
- //   }
- // }
   return 1;
 }
 
-void parseCmd(struct Cmd *cmd) {
+int parseCmd(struct Cmd *cmd) {
   int word_index=0;
   int char_index=0;
-  int prev_space=1;
+  int prev_space=0;
   char c;
 
-  while((c=getchar()) !='\n' && c != ';') {
+  while((c=getchar()) !='\n' && c != ';' ) {
+    if(feof(stdin)) {
+      return 1;
+    }
     if (isspace(c)) {
       if (!prev_space) {
         word_index++;
         char_index=0;
-      }
+      } 
       prev_space=1;
     } else {
-      cmd->argv[word_index][char_index] = c;
+      if(word_index==0) {
+        cmd->executable[char_index]=c;
+      } else {
+        cmd->argv[word_index-1][char_index] = c;
+      }
       char_index++;
     }
   }
+  return 0;
 }
 
 int mystrncpy(char *dest,char *src,size_t n) {
